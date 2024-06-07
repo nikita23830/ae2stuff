ce2e104acc862fdcb4048628c48d554a202db3f1
ReignOfFROZE <reignoffroze@hotmail.com>
2024-06-07 12:01:38 -0400

    Added NBT clearing recipe for basic wireless kit and spotless

--- src/main/scala/net/bdew/ae2stuff/Recipes.scala
+++ src/main/scala/net/bdew/ae2stuff/Recipes.scala
@@ -1,15 +1,19 @@
 package net.bdew.ae2stuff
 
 import cpw.mods.fml.common.registry.GameRegistry
-import net.bdew.ae2stuff.items.AdvWirelessKit
+import net.bdew.ae2stuff.items.{AdvWirelessKit, ItemWirelessKit}
 import net.minecraft.item.ItemStack
 
 object Recipes {
   def load(): Unit = {
-    //add recipe to clear NBT from adv wireless kit
+    // add recipe to clear NBT from adv wireless kit
     GameRegistry.addShapelessRecipe(
       new ItemStack(AdvWirelessKit),
       AdvWirelessKit
+    )
+    GameRegistry.addShapelessRecipe(
+      new ItemStack(ItemWirelessKit),
+      ItemWirelessKit
     )
   }
 }
